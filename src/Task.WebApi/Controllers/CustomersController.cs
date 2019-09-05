3010027a08ba0239436d5b48aa7fd44294188e40
alinaladybug23@gmail.com <alinaladybug23@gmail.com>
2019-09-05 19:13:16 +0300

    Refactor sln architecture

---
+++ src/Task.WebApi/Controllers/CustomersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using Task.BL.Abstract;
+
+namespace Task.WebApi.Controllers
+{
+    [ApiController]
+    [Route("customers")]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ICustomerService _customerService;
+
+        public CustomersController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+
+        [HttpGet]
+        public async Task<JObject> GetCustomersByNameAsync(string customerName)
+        {
+            return await _customerService.GetCustomersByNameAsync(customerName);
+        }
+    }
+}
